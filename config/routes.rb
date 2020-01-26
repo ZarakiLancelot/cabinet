d5f8bbfdec781757f19b7e1644c98e0ffded63a4
ZarakiLancelot <kenpachi2783@gmail.com>
2020-01-26 09:10:17 -0600

    se cambia la ruta principal de la aplicación

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
 
   root 'welcome#index'
 end
